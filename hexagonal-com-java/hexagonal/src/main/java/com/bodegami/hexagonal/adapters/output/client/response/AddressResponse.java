5032b9ca76bb7437217d9d32bd7217826671282a
Renato <rena.f.oliveira@gmail.com>
2024-02-14 23:09:19 -0300

    criando o client

---
+++ hexagonal-com-java/hexagonal/src/main/java/com/bodegami/hexagonal/adapters/output/client/response/AddressResponse.java
@@ -0,0 +1,13 @@
+package com.bodegami.hexagonal.adapters.output.client.response;
+
+import lombok.Data;
+
+@Data
+public class AddressResponse {
+
+    private String street;
+    private String city;
+    private String state;
+
+
+}
