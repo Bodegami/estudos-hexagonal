1a9ece9bc7d86ae058437bab4f5c01d80389eccc
Renato <rena.f.oliveira@gmail.com>
2024-02-18 19:49:46 -0300

    garantindo que as classes estejam nos pacotes corretos

--- hexagonal-com-java/hexagonal/src/test/java/com/bodegami/hexagonal/architecture/LayeredArchitectureTest.java
+++ hexagonal-com-java/hexagonal/src/test/java/com/bodegami/hexagonal/architecture/LayeredArchitectureTest.java
@@ -3,24 +3,23 @@ package com.bodegami.hexagonal.architecture;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
 
 @AnalyzeClasses(packages = "com.bodegami.hexagonal")
-@SpringBootTest
 public class LayeredArchitectureTest {
 
+    //Valida se o acesso as camadas está sendo respeitado
     @ArchTest
     public static final ArchRule layered_architecture_test = layeredArchitecture()
             .consideringAllDependencies()
-            .layer("AdaptersIn").definedBy("..adapters.input..")
+            .layer("AdaptersIn").definedBy("..adapters.input..") // definicao de alias ao diretorio informado
             .layer("AdaptersOut").definedBy("..adapters.output..")
             .layer("UseCase").definedBy("..application.core.usecase..")
             .layer("PortsIn").definedBy("..application.ports.input..")
             .layer("PortsOut").definedBy("..application.ports.output..")
             .layer("Config").definedBy("..config..")
-            .whereLayer("AdaptersIn").mayOnlyBeAccessedByLayers("Config")
+            .whereLayer("AdaptersIn").mayOnlyBeAccessedByLayers("Config") //valida se alias só pode ser acessado pelo pacote informado
             .whereLayer("AdaptersOut").mayOnlyBeAccessedByLayers("Config")
             .whereLayer("UseCase").mayOnlyBeAccessedByLayers("Config")
             .whereLayer("PortsIn").mayOnlyBeAccessedByLayers("UseCase", "AdaptersIn", "Config")
---
+++ hexagonal-com-java/hexagonal/src/test/java/com/bodegami/hexagonal/architecture/NamingConventionTest.java
@@ -0,0 +1,47 @@
+package com.bodegami.hexagonal.architecture;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+@AnalyzeClasses(packages = "com.bodegami.hexagonal")
+public class NamingConventionTest {
+
+    //Verificando se as classes estão nos diretorios corretos
+    @ArchTest
+    public static final ArchRule consumer_reside_only_consumer_package = classes()
+            .that()
+            .haveNameMatching(".*Consumer") //Sufixo a ser verificado
+            .should()
+            .resideInAPackage("..adapters.input.consumer") //diretorio onde deve residir as classes
+            .as("Consumer classes reside inside consumer package in adapters.input package"); //msg erro personalizada
+
+    @ArchTest
+    public static final ArchRule mapper_reside_only_mapper_package = classes()
+            .that()
+            .haveNameMatching(".*Mapper")
+            .should()
+            .resideInAnyPackage("..adapters.input.consumer.mapper", "..adapters.input.controller.mapper",
+                    "..adapters.output.client.mapper", "..adapters.output.repository.mapper")
+            .as("Mapper classes should reside inside mapper package in controller, client, consumer and repository packages");
+
+    @ArchTest
+    public static final ArchRule message_reside_only_message_package = classes()
+            .that()
+            .haveNameMatching(".*Message")
+            .should()
+            .resideInAPackage("..adapters.input.consumer.message")
+            .as("Message classes should reside inside message package in adapters.input.consumer.message package");
+
+    @ArchTest
+    public static final ArchRule controller_reside_only_controller_package = classes()
+            .that()
+            .haveNameMatching(".*Controller")
+            .should()
+            .resideInAPackage("..adapters.input.controller")
+            .as("Controller classes should reside inside controller package in adapters.input.controller package");
+    
+
+}
